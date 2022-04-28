98a5d0eb4d16a8465a0b1a1f168ef8534533e9f3
SaileshAman <87354977+SaileshAman@users.noreply.github.com>
2022-04-28 11:34:03 +0530

    Second Commit

--- deleteServ.php
+++ deleteServ.php
@@ -4,29 +4,30 @@ include 'config.php';
 session_start();
 
 $sid = $_POST['sid'];
+$
 $custid = "";
 $sqlBase = "SELECT * FROM services WHERE servid='$sid'";
 $resultBase = mysqli_query($conn, $sqlBase);
 if($row = mysqli_fetch_assoc($resultBase)) {
     $cid = $row['custid'];  $mid = $row['mechid'];
+    $sql3 = "DELETE FROM services WHERE servid='$sid'";
+    $result3 = mysqli_query($conn, $sql3);
     
-    $sql1 = "SELECT * FROM customers WHERE custif='$cid'";
+    $sql1 = "SELECT * FROM services WHERE custid='$cid'";
 	$result1 = mysqli_query($conn, $sql1);
-    $row1 = mysqli_fetch_assoc($result1);
-    $cnt = $row1['requests']; $cnt--;
+    $cnt = mysqli_num_rows($result1);
     $sql1 = "UPDATE customers SET requests='$cnt' WHERE custid='$cid'";
 	$result1 = mysqli_query($conn, $sql1);
 
-    $sql2 = "SELECT * FROM mechanics WHERE mechid='$mid'";
+    $sql2 = "SELECT * FROM servies WHERE mechid='$mid'";
 	$result2 = mysqli_query($conn, $sql2);
-    $row2 = mysqli_fetch_assoc($result2);
-    $cnt = $row2['services']; $cnt--;
+    $row2 = mysqli_num_rows($result2);
     $sql2 = "UPDATE mechanics SET services='$cnt' WHERE mechid='$mid'";
 	$result2 = mysqli_query($conn, $sql2);
-
-    $sql3 = "DELETE FROM services WHERE servid='$sid'";
-    $result3 = mysqli_query($conn, $sql3);
 }
 
-header("Location: cust.php");
+if (isset($_SESSION['email']) && $_SESSION['type'] == "customers")
+    header("Location: cust.php");
+else if (isset($_SESSION['email']) && $_SESSION['type'] == "mechanics")
+    header("Location: mech.php");
 ?>
